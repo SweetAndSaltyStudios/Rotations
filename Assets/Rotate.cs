8865dc5310f40701e50fe81e5e0132bd52c32135
Mathias Mustonen <mathias.mustonen@gmail.com>
2020-08-08 22:47:24 +0300

    Project cleaning

---
+++ Assets/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public GameObject CubeRed, CubeBlue;
+
+    private float zOffset = 1.5f;
+
+    private void Update()
+    {
+        RotateCube();
+    }
+
+    void RotateCube()
+    {
+        CubeRed.transform.position =
+            new Vector3(CubeBlue.transform.position.x, CubeRed.transform.position.y, CubeBlue.transform.position.z) + CubeBlue.transform.forward * zOffset;
+
+        CubeRed.transform.LookAt(
+
+            new Vector3(CubeBlue.transform.position.x, CubeRed.transform.position.y, CubeBlue.transform.position.z)
+
+            );
+    }
+}
